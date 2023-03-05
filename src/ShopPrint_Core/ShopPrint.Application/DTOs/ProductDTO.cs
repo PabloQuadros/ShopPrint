7273937408193b894b95fe13aa1d735d60845aca
Pablo Quadros <pablo.aquadros@gmail.com>
2023-03-05 15:02:13 -0300

    Application DTOs class

---
+++ src/ShopPrint_Core/ShopPrint.Application/DTOs/ProductDTO.cs
@@ -0,0 +1,45 @@
+﻿using ShopPrint.Domain.Entities;
+using ShopPrint.Domain.Validation.Product;
+using ShopPrint.Domain.Validation.Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopPrint.Application.DTOs
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+
+        [NameValidation]
+        [DisplayName("Nome do Produto")]
+        public string Name { get; set; }
+
+        [ProductDescriptionValidation]
+        [DisplayName("Descrição")]
+        public string Description { get; set; }
+
+        [Required]
+        [Range(0.1, 1000000, ErrorMessage = "O preço deve estar entre 0,1 e 1.000.000,00.")]
+        [DisplayName("Preço")]
+        public double Price { get; set; }
+
+        [Required]
+        [Range(0, 1000000, ErrorMessage = "O estoque deve estar entre 0 e 1.000.000.")]
+        [DisplayName("Estoque")]
+        public int Stock { get; set; }
+
+        [UrlValidation]
+        [DisplayName("Url da Imagem")]
+        public string Image { get; set; }
+
+        [Required(ErrorMessage = "Necessário informar uma categoria.")]
+        [DisplayName("Categoria")]
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
