ffb0fd86b546569c8036775d96a5e0054b4e7cc9
Pablo Quadros <pablo.aquadros@gmail.com>
2023-03-05 23:05:39 -0300

    .

--- src/ShopPrint_Core/ShopPrint.Domain/Entities/Checkout.cs
+++ src/ShopPrint_Core/ShopPrint.Domain/Entities/Checkout.cs
@@ -1,6 +1,7 @@
 ﻿using ShopPrint.Domain.Enum;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +11,11 @@ namespace ShopPrint.Domain.Entities
     public class Checkout
     {
         public int Id { get; set; }
+        public int UserId { get; set; }
+        public virtual IUser user { get; set; }
         public int OrderId { get; set; }
         public virtual Order order { get; set; }
         public PaymentOption paymentOption { get; set; }
-        public string Description { get; set; }
         public DateTime dateTime { get; set; }
         public string country { get; set; }
         public string city { get; set; }
@@ -22,6 +24,10 @@ namespace ShopPrint.Domain.Entities
         public string number { get; set; }
         public string complement { get; set; }
         public string zipcode { get; set; }
+        public string cardNumber { get; set; }
+        public string cardName { get; set; }
+        public string cardType { get; set; }
+        public string cvv { get; set; }
 
     }
 }
