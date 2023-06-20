9c0809cebd7d03844cdc1c82a871a1457c51db5d
PabloQuadros <pablo.quadros@allog.com.br>
2023-06-20 18:02:20 -0300

    .

--- src/ShopPrint/ShopPrint/ShopPrint_API/Entities/DTOs/PixDTO.cs
+++ src/ShopPrint/ShopPrint/ShopPrint_API/Entities/DTOs/PixDTO.cs
@@ -1,6 +1,10 @@
-﻿namespace ShopPrint_API.Entities.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShopPrint_API.Entities.DTOs;
 public class PixDTO : PaymentDTO
 {
+    [Required(ErrorMessage = "O campo CPF é obrigatório.")]
+    [RegularExpression(@"^\d{3}\.\d{3}\.\d{3}-\d{2}$", ErrorMessage = "O formato do CPF é inválido.")]
     public string CPF { get; set; }
     public string? pixCode { get; set; }
 }
